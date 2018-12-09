113fc66940bd1ea59f7c0b14f1547eb5ebce498e
afeiship <afeiship@gmail.com>
2018-12-09 23:47:52 +0800

    add: comments for export NODE_PATH

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-## for yeoman warning:
+## for yeoman warning: (Make sure your NODE_PATH matches the npm root)
 export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
 alias yo-iwa-ctrl='yo ionic-wechat-app:controller';
