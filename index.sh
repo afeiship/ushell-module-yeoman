f935807b005796ab213ec6bb673cda9de93690d6
afeiship <afeiship@gmail.com>
2018-12-09 23:45:48 +0800

    add: export for yeoman warning

--- index.sh
+++ index.sh
@@ -2,6 +2,8 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+## for yeoman warning:
+export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
 alias yo-iwa-ctrl='yo ionic-wechat-app:controller';
 alias yo-iwa-widget='yo ionic-wechat-app:component';
