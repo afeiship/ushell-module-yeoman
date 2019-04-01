c964bd0f0597c53cc70166fd328038f49b5181d9
afeiship <1290657123@qq.com>
2019-04-01 21:42:19 +0800

    add: yeom alias

--- index.sh
+++ index.sh
@@ -5,7 +5,6 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 ## for yeoman warning: (Make sure your NODE_PATH matches the npm root)
 export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
-alias yo-iwa-ctrl='yo ionic-wechat-app:controller';
-alias yo-iwa-widget='yo ionic-wechat-app:component';
+alias yo-cache='cd $HOME/.cache/node-yeoman-remote-cache';
 
 unset ROOT_PATH;
