daba747787dd372c30d8ab2c3acd08c222ad7777
afeiship <1290657123@qq.com>
2019-08-22 21:21:46 +0800

    add: update clean-x

--- index.sh
+++ index.sh
@@ -8,4 +8,7 @@ export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 alias yo-cache='cd $HOME/.cache/node-yeoman-remote-cache';
 alias yo-clean='cd $HOME/.cache/node-yeoman-remote-cache && rm -rf afeiship && cd -'
 
+# just for learning
+alias yo-clean-x='(cd $HOME/.cache/node-yeoman-remote-cache && rm -rf afeiship)&'
+
 unset ROOT_PATH;
